3530b7d55fccdf0d75a2238fefed2bb9a258e41d
Darya <daryalas17@gmail.com>
2022-08-06 07:25:50 +0300

    seminar4_1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число А: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int B = int.Parse(Console.ReadLine());
+
+double AB(int aLoc, int bLoc){
+
+    double rez = Math.Pow((aLoc), bLoc);
+    return rez;
+
+}
+
+Console.WriteLine(AB(A,B));
